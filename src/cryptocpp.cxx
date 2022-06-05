f3b75a0098cc5dd45f894276cb3708f417a7148e
alireza chegini <ali.chegini.1990@gmail.com>
2022-06-05 11:06:15 +0430

    Add enc and dec function

--- src/cryptocpp.cxx
+++ src/cryptocpp.cxx
@@ -1,4 +1,17 @@
 #include "cryptocpp.h"
 
+BSDCrypt::BSDCrypt(){}
+
+BSDCrypt::~BSDCrypt(){}
+
+string BSDCrypt::Encrypt(const string &inp){
+
+}
+
+bool BSDCrypt::Compare(const string &a, const string &b){
+
+}
+
+
 
 
--- src/cryptocpp.h
+++ src/cryptocpp.h
@@ -1,9 +1,31 @@
 #ifndef _CRYPTOCPP_H_
 #define _CRYPTOCPP_H_
 
+#include <string>
 
+/**
+ * Using BSD crypt for password hashing
+ *
+ */
 
+class BSDCrypt {
 
+public:
+  BSDCrypt();
+  ~BSDCrypt();
 
+  BSDCrypt(const BSDCrypt &) = delete;
+  BSDCrypt &operator=(const BSDCrypt &) = delete;
+
+  BSDCrypt(BSDCrypt &&) = delete;
+  BSDCrypt &operator=(BSDCrypt &&) = delete;
+
+  
+  string Encrypt(const string &inp);
+  
+  
+  bool Compare(const string &a, const string &b);
+
+};
 
 #endif
