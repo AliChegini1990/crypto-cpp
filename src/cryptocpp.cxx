f08a33ca91e00b9dbaf19dfb93301d703a42db1e
alireza chegini <ali.chegini.1990@gmail.com>
2022-06-05 12:28:04 +0430

    Develop bcrypt encryption

--- src/cryptocpp.cxx
+++ src/cryptocpp.cxx
@@ -1,17 +1,59 @@
 #include "cryptocpp.h"
 
-BSDCrypt::BSDCrypt(){}
+BSDCrypt::BSDCrypt(){
+  
+  // set default encryption
+  setType(EncryptionType::bcrypt);
+}
 
-BSDCrypt::~BSDCrypt(){}
+BSDCrypt::~BSDCrypt() {}
 
-string BSDCrypt::Encrypt(const string &inp){
+string BSDCrypt::Encrypt(const string &inp) {
+
+  struct crypt_data data {
+    .initialized = 0
+  };
+  // data.initialized = 0;
+  memset(&data, 0, sizeof(data));
+
+  // char *crypt_r(const char *phrase, const char *setting, struct crypt_data
+  // *data);
+
+  // struct crypt_data {
+  //  char output[CRYPT_OUTPUT_SIZE];
+  //  char setting[CRYPT_OUTPUT_SIZE];
+  //  char phrase[CRYPT_MAX_PASSPHRASE_SIZE];
+  //  char initialized;
+  //};
+
+  // char *
+  //  crypt_gensalt_rn(const char * prefix, unsigned long count, const char
+  //  *rbytes, int nrbytes, char * output, int output_size);
+
+
+
+
+// hash based on the Blowfish block cipher, modified to have an extra-expensive key schedule.  Originally developed by Niels Provos and David Mazieres
+//     for OpenBSD and also supported on recent versions of FreeBSD and NetBSD, on Solaris 10 and newer, and on several GNU/*/Linux distributions.
+//
+//     Prefix
+//         "$2b$"
+//
+//     Hashed passphrase format
+//         \$2[abxy]\$[0-9]{2}\$[./A-Za-z0-9]{53}
+//
+//     Maximum passphrase length
+//         72 characters
+//
+//     Hash size
+//         184 bits
+//
+//     Salt size
+//         128 bits
+//
+//     CPU time cost parameter
+//         4 to 31 (logarithmic)
 
 }
 
-bool BSDCrypt::Compare(const string &a, const string &b){
-
-}
-
-
-
-
+bool BSDCrypt::Compare(const string &a, const string &b) {}
--- src/cryptocpp.h
+++ src/cryptocpp.h
@@ -2,11 +2,14 @@
 #define _CRYPTOCPP_H_
 
 #include <string>
+#include <unordered_map>
 
 /**
  * Using BSD crypt for password hashing
  *
  */
+
+#include <crypt.h>
 
 class BSDCrypt {
 
@@ -20,12 +23,60 @@ public:
   BSDCrypt(BSDCrypt &&) = delete;
   BSDCrypt &operator=(BSDCrypt &&) = delete;
 
-  
-  string Encrypt(const string &inp);
-  
-  
-  bool Compare(const string &a, const string &b);
+  /**
+   * A list of supported encryption types
+   */
+  enum class EncryptionType{
+    yescrypt,
+    gost-yescrypt,
+    scrypt,
+    bcrypt,
+    sha512crypt,
+    sha256crypt,
+    sha1crypt,
+    SunMD5,
+    md5crypt,
+    bsdicrypt,
+    bigcrypt,
+    descrypt,
+    NT
+  };
 
+  /**
+   * Set an encryption type
+   *
+   * @param type encryption type
+   */
+  inline void setType(EncryptionType type) noexcept {type_ = type;}
+  
+  /**
+   * Encrypt the input string
+   * 
+   * @param inp input value
+   * @return An encrypted string. If error occured this function throw an exception.
+   *
+   */
+  string Encrypt(const string &inp);
+
+  /**
+   * Compare encrpted string to plain text
+   *
+   * @param enc_a encrypted string
+   * @param plain_b palin text 
+   * @return Return true if they are equals
+   *
+   */
+  bool Compare(const string &enc_a, const string &plain_b);
+  
+
+  private:
+
+   EncryptionType type_;
+
+   unordered_map<string, EncryptionType> enc_text{
+    {"$y$", EncryptionType::bcrypt}
+   }
 };
+
 
 #endif
