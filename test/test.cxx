3562f17b9e5d1e2c5e8f84a933613fb110e07b4f
alireza chegini <ali.chegini.1990@gmail.com>
2022-06-06 16:08:27 +0430

    test.cxx: Update

--- test/test.cxx
+++ test/test.cxx
@@ -2,12 +2,15 @@
 #include <cassert>
 #include <stdexcept>
 #include <iostream>
+#include <regex>
+#include <cassert>
 
 using namespace std;
 
-void test_bcrypt_block() {
+void test_bcrypt_block_1() {
 
   cout << "test1 is running" << endl;
+  cout << "================" << endl;
   string input = "test1";
  
   try {
@@ -17,16 +20,71 @@ void test_bcrypt_block() {
     
     string out = crypt.Encrypt(input);
     cout << "output: " << out  << endl;
-  
-    cout << "error: " << to_string(errno) << endl;
-    cout << "error msg: " << strerror(errno) << endl;
-   
+ 
+   //verify output
+   auto bc_ = crypt.getInternalEncryption(); 
+   auto base =  dynamic_cast<Bcrypt&>(*bc_);
+   std::regex re(base.regexCheck());
+   out = base.prefix() + to_string(base.cpuCost()) + "$" + out;
+   cout << "setting + encoded string : " << out  << endl;
+
+   if(!std::regex_match(out, re)){
+     cerr << "Encryption Failed: out put is not correct" << endl;
+     throw runtime_error{"Regex is not match"};
+   }
+   cout << "Successful" << endl;   
+   cout << "==========" << endl;
+
   } catch (const std::exception &ex) {
+    cerr << "error: " << to_string(errno) << endl;
+    cerr << "error msg: " << strerror(errno) << endl;
     cerr << ex.what() << endl;
+    cerr << "Failed" << endl;   
+    cerr << "======" << endl;
+
+    assert(1==0);
   }
 }
+void test_bcrypt_block_2() {
 
+  cout << "test2 is running" << endl;
+  cout << "================" << endl;
+  string input = "";
+ 
+  try {
+
+    BSDCrypt crypt(BSDCrypt::EncryptionType::bcrypt);
+    cout << "input: " << input  << endl;
+    
+    string out = crypt.Encrypt(input);
+    cout << "output: " << out  << endl;
+ 
+   //verify output
+   auto bc_ = crypt.getInternalEncryption(); 
+   auto base =  dynamic_cast<Bcrypt&>(*bc_);
+   std::regex re(base.regexCheck());
+   out = base.prefix() + to_string(base.cpuCost()) + "$" + out;
+   cout << "setting + encoded string : " << out  << endl;
+
+   if(!std::regex_match(out, re)){
+     cerr << "Encryption Failed: out put is not correct" << endl;
+     throw runtime_error{"Regex is not match"};
+   }
+   cout << "Successful" << endl;   
+   cout << "==========" << endl;
+
+  } catch (const std::exception &ex) {
+    cerr << "error: " << to_string(errno) << endl;
+    cerr << "error msg: " << strerror(errno) << endl;
+    cerr << ex.what() << endl;
+    cerr << "Failed" << endl;   
+    cerr << "======" << endl;
+
+    assert(1==0);
+  }
+}
 int main() { 
-  test_bcrypt_block();
+  test_bcrypt_block_1();
+  test_bcrypt_block_2();
   return 0; 
 }
